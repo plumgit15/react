0d181df3df3abb3a4ef90ef2571bd6b41ae17a22
plumgit15 <63837262+plumgit15@users.noreply.github.com>
2021-10-20 09:24:17 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react
+# todo-react
 Created with CodeSandbox
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello 学校Cデジ開本部本部会</h1>
+      <h1>React紹介中</h1>
+    </div>
+  );
+}
